b0ec2148bfdff19fcaa1296915a7354cf227870d
vina786 <vinay76b@gmail.com>
2024-12-07 06:26:31 +0530

    Add files via upload

---
+++ hackerman.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class hackerman {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            int sum = A + B;
+            if (isPrime(sum)) {
+                System.out.println("ALICE");
+            } else {
+                System.out.println("BOB");
+            }
+        }
+
+        scanner.close();
+    }
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
