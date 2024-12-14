e80a22a63e5d0a193d92653e34ee7fcc7d459f7a
B VINAY <vinay76b@gmail.com>
2024-12-14 15:52:08 +0530

    Add files via upload

---
+++ thalaforreason.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class thalaforreason {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a==7)
+		{
+		    System.out.println("THALA");
+		}
+		else{
+		    System.out.println("SADGE");
+		}
+
+	}
+}
+
